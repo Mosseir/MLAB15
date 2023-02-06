5d01789658ea274b715513ec34391f6193fbfa8e
Mosseir <glock18_123@hotmail.com>
2023-02-06 21:00:58 +0700

    Done

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -22,19 +22,31 @@ int main(){
 
 void inputMatrix(double m[][N]){
 	for(int i = 0; i < N ; i++){
-		cout << "Row " << i+1 << ":";
+		cout << "Row " << i+1 << ": ";
 		for(int j = 0; j < N; j++){
 			cin >> m[i][j] ;
 		}
 	}
 }
 
-void findLocalMax(const double [][N], bool [][N]){
-
+void findLocalMax(const double x[][N], bool y[][N]){
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			if ((i > 0 && i <= N - 2) && (j > 0 && j <= N - 2)){
+				if ((x[i][j] >= x[i - 1][j]) && (x[i][j] >= x[i][j - 1]) && (x[i][j] >= x[i][j + 1]) && (x[i][j] >= x[i + 1][j])){
+					y[i][j] = 1;
+				}else y[i][j] = 0;
+				}else y[i][j] = 0;
+		}
+	}
 }
 
 void showMatrix(const bool s[][N]){
 	for(int i = 0; i < N ; i++){
-		
+		for (int j = 0; j < N; j++)
+		{
+			cout << s[i][j] << " ";
+		}
+		cout << endl;
 	}
 }
